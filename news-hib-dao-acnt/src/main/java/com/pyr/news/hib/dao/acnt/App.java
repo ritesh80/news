d35a6853fd223bd7c6305137790beb23592afd64
unknown <riteshku@INA428PS1L.pyramidconsultinginc.com>
2014-11-03 15:12:42 +0530

    first commit

---
+++ news-hib-dao-acnt/src/main/java/com/pyr/news/hib/dao/acnt/App.java
@@ -0,0 +1,13 @@
+package com.pyr.news.hib.dao.acnt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
